d29748e108227a059603c5e9e1050ab9b816cb4d
Javinator9889 <javialonso007@hotmail.es>
2018-02-10 15:29:25 +0100

    Created "toObjectFile.py"
    Use this method from command line for creating a bytes-object serializated with your Telegram TOKEN

--- APP/toObjectFile.py
+++ APP/toObjectFile.py
@@ -1,6 +1,10 @@
 import sys
 import pickle
 
+''' Use this file for initializing your token file.
+    In command line, write: "python toObjectFile.py <YOUR_TELEGRAM_TOKEN>" (without < >)
+    And your file will be created for using the bot
+'''
 telegram = {'TOKEN': sys.argv[1]}
 with open("TOKEN.dat", "wb") as file:
     pickle.dump(telegram, file)
